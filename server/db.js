7abfb6239b387fa89cb4c059ae291c849a9dd462
Katie B <kblatherwick@productscape.tech>
2024-05-20 15:47:49 -0600

    maybe all i can get done, i have too many kids with stuff going on

--- server/db.js
+++ server/db.js
@@ -1,3 +1,6 @@
+//hide the goods
+require("dotenv").config();
+//create a db client
 const pg = require("pg");
 const client = new pg.Client(
   process.env.DATABASE_URL || "postgres://localhost/items_block37"
@@ -19,23 +22,21 @@ const createTables = async () => {
     CREATE TABLE users(
         id UUID PRIMARY KEY,
         username VARCHAR(100) UNIQUE NOT NULL,
-        displayname VARCHAR(100),
-        password VARCHAR(100) NOT NULL,
-        token VARCHAR(100)
-    );
+        password VARCHAR(100) NOT NULL
+      );
 
     CREATE TABLE items(
         id UUID PRIMARY KEY,
         name VARCHAR(100) UNIQUE NOT NULL,
         category VARCHAR(100) NOT NULL,
-        price NUMERIC (3,2) NOT NULL,
-        avgScore NUMERIC(3, 2)
+        price NUMERIC (7,2) NOT NULL,
+        avgScore NUMERIC(4, 2)
     );
 
     CREATE TABLE reviews(
         id UUID PRIMARY KEY,
         review VARCHAR(1000) NOT NULL,
-        score INTEGER NOT NULL CHECK (score >= 0 AND score <= 100),
+        score INTEGER NOT NULL CHECK (score >= 0 AND score <= 10),
         itemId UUID REFERENCES items(id),
         userId UUID REFERENCES users(id),
         CONSTRAINT unique_review UNIQUE (itemId, userId)
@@ -43,10 +44,9 @@ const createTables = async () => {
     CREATE TABLE reviewComments(
         id UUID PRIMARY KEY,
         comment VARCHAR(1000) NOT NULL,
-        itemId UUID REFERENCES items(id),
+        itemsId UUID REFERENCES items(id),
         userId UUID REFERENCES users(id),
-        reviewId UUID REFERENCES reviews(id),
-        CONSTRAINT unique_comment UNIQUE (itemId, userId,reviewId)
+        reviewId UUID REFERENCES reviews(id)
     );
 `;
   await client.query(SQL);
@@ -54,60 +54,254 @@ const createTables = async () => {
 
 //create user in db from code
 const createUser = async ({ username, password }) => {
-  try {
-    const SQL = `--sql
-    INSERT INTO users(id, username, password, displayname, token)
-    VALUES ($1, $2, $3, $4, $5)
-    RETURNING *;
-    `;
-    const response = await client.query(SQL, [
-      uuid.v4(),
-      username,
-      await bcrypt.hash(password, 10),
-    ]);
-    return response.rows[0];
-  } catch (error) {
-    throw error;
-  }
+  const SQL = `--sql
+      INSERT INTO users(id, username, password)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+      `;
+  const response = await client.query(SQL, [
+    uuid.v4(),
+    username,
+    await bcrypt.hash(password, 2),
+  ]);
+  return response.rows[0];
 };
 
 //create item in db from code
 const createItem = async ({ name, category, price, avgScore }) => {
-  try {
-    const SQL = `--sql
+  const SQL = `--sql
     INSERT INTO items(id, name, category, price, avgScore)
     VALUES ($1, $2, $3, $4, $5)
     RETURNING *;
     `;
-    const response = await client.query(SQL, [
-      uuid.v4(),
-      name,
-      category,
-      price,
-      avgScore,
-    ]);
-    return response.rows[0];
-  } catch (error) {
+  const response = await client.query(SQL, [
+    uuid.v4(),
+    name,
+    category,
+    price,
+    avgScore,
+  ]);
+  return response.rows[0];
+};
+
+//create review in db from code, oy
+const createReview = async ({ review, score, itemId, userId }) => {
+  const SQL = `--sql
+    INSERT INTO reviews(id, review, score, itemId, userId)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING *;
+    `;
+  const response = await client.query(SQL, [
+    uuid.v4(),
+    review,
+    score,
+    itemId,
+    userId,
+  ]);
+  return response.rows[0];
+};
+
+//create a mechanism to update a review
+const updateReview = async ({ review, score, userId, id }) => {
+  // console.log("review", review, "score", score, "userId", userId);
+  const SQL = `--sql
+    UPDATE reviews
+    SET review = $1, score = $2
+    WHERE userId = $3 and id = $4
+    RETURNING *;
+    `;
+  const response = await client.query(SQL, [review, score, userId, id]);
+  return response.rows[0];
+};
+
+//delete a review that I created
+const deleteReview = async ({ id, userId }) => {
+  const dependentComments = await client.query(
+    `SELECT COUNT(*) FROM reviewComments WHERE reviewId = $1`,
+    [id]
+  );
+  if (dependentComments.rows[0].count > 0) {
+    throw new Error("Cannot delete review with dependent comments");
+  }
+  const SQL = `--sql
+    DELETE FROM reviews
+    WHERE id = $1 and userId = $2
+    `;
+  await client.query(SQL, [id, userId]);
+};
+
+//create reviewComments in db from code, oy
+const createReviewComment = async ({ comment, itemsId, userId, reviewId }) => {
+  const SQL = `--sql
+    INSERT INTO reviewComments(id, comment, itemsId, userId, reviewId)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING *;
+    `;
+  const response = await client.query(SQL, [
+    uuid.v4(),
+    comment,
+    itemsId,
+    userId,
+    reviewId,
+  ]);
+  return response.rows[0];
+};
+
+//delete reviewComments in db from code, oy
+const deleteReviewComment = async ({ id, userId }) => {
+  const SQL = `--sql
+    DELETE FROM reviewComments
+    WHERE id = $1 and userId = $2
+    RETURNING *;
+    `;
+  await client.query(SQL, [id]);
+};
+//authenticate user
+const authenticate = async ({ username, password }) => {
+  const SQL = `--sql
+      SELECT id, password
+      FROM users
+      WHERE username = $1
+      `;
+  const response = await client.query(SQL, [username]);
+  if (
+    !response.rows.length ||
+    (await bcrypt.compare(password, response.rows[0].password)) === false
+  ) {
+    const error = Error("you know you aint authorized");
+    error.status = 401;
     throw error;
   }
+  const token = await jwt.sign({ id: response.rows[0].id }, JWT);
+  console.log("this is token from db authenticate", token);
+  return { token };
+};
+
+//find a user with a token
+const findUserByToken = async (token) => {
+  let id;
+  try {
+    const payload = await jwt.verify(token, JWT);
+    console.log(`payload`, payload);
+    id = payload.id;
+  } catch (err) {
+    const error = Error("you know you aint authorized");
+    error.status = 401;
+    throw error;
+  }
+  const SQL = `--sql
+        SELECT * FROM users WHERE id = $1
+        `;
+  //use id instead of token, eh?
+  const response = await client.query(SQL, [id]);
+  if (!response.rows.length) {
+    const error = Error("you know you aint authorized");
+    error.status = 401;
+    throw error;
+  }
+  return response.rows[0];
+};
+
+//--------------------fetching data from db--------------------
+//fetch users
+const fetchUsers = async () => {
+  const SQL = `--sql
+    SELECT * FROM users;
+    `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+//fetch ALL items (no login)
+const fetchItems = async () => {
+  const SQL = `--sql
+    SELECT * FROM items;
+    `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+//fetch a SINGLE item  (no login)
+const fetchItem = async (id) => {
+  const SQL = `--sql
+    SELECT * FROM items
+    where id = $1
+    `;
+  const response = await client.query(SQL, [id]);
+  return response.rows;
+};
+
+//------ALL---------fetch all reviews
+const fetchReviews = async () => {
+  const SQL = `--sql
+    SELECT * FROM reviews;
+    `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+//-----ONE item's reviewS------fetch all reviews for an item
+const fetchItemReviews = async (itemId) => {
+  const SQL = `--sql
+    SELECT * FROM reviews
+    WHERE itemId = $1;
+    `;
+  const response = await client.query(SQL, [itemId]);
+  return response.rows;
+};
+
+//------ONE SINGLE REVIEW, who cares about the item id ... hahaha we do
+const fetchSingleReview = async ({ itemId, id }) => {
+  // console.log("itemId", itemId, "id", id);
+  const SQL = `--sql
+    SELECT * FROM reviews
+    WHERE itemId = $1 and id = $2;
+    `;
+  const response = await client.query(SQL, [itemId, id]);
+  return response.rows[0];
+};
+
+//--------ALL - fetch all review comments
+fetchReviewComments = async () => {
+  const SQL = `--sql
+    SELECT * FROM reviewComments;
+    `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+//--------ONE - fetch all JUST MY reviews
+fetchMyReviews = async (userId) => {
+  const SQL = `--sql
+    SELECT * FROM reviews
+    WHERE userId = $1;
+    `;
+  const response = await client.query(SQL, [userId]);
+  return response.rows;
 };
 
 module.exports = {
   client,
   createTables,
-  //   createUser,
-  //   createItem,
-  //   createReviewComment,
-  //   createReview,
-  //   fetchUsers,
-  //     fetchItems,
-  //     fetchComments,
-  //     fetchReviews,
-  //     authenticate,
-  //     findUserWithToken,
-  //     createUserAndGenerateToken,
-  //     deleteComment,
-  //     deleteReview,
+  createUser,
+  createItem,
+  createReview,
+  createReviewComment,
+  fetchUsers,
+  fetchItems,
+  fetchReviews,
+  fetchItemReviews,
+  fetchSingleReview,
+  fetchReviewComments,
+  deleteReviewComment,
+  fetchItem,
+  fetchMyReviews,
+  updateReview,
+  deleteReview,
+  //   deleteReview,
   //     updateComment,
   //     updateReview,
+  authenticate,
+  findUserByToken,
+  //     createUserAndGenerateToken,
 };
--- server/index.js
+++ server/index.js
@@ -4,30 +4,403 @@ const {
   createTables,
   createUser,
   createItem,
-  createComment,
   createReview,
+  createReviewComment,
   fetchUsers,
   fetchItems,
-  fetchComments,
   fetchReviews,
-  authenticate,
-  findUserWithToken,
-  createUserAndGenerateToken,
-  deleteComment,
-  deleteReview,
-  updateComment,
+  fetchItemReviews,
+  fetchSingleReview,
+  fetchMyReviews,
   updateReview,
+  deleteReview,
+  fetchReviewComments,
+  deleteReviewComment,
+  fetchItem,
+  //   deleteReview,
+  //     updateComment,
+  //     updateReview,
+  authenticate,
+  findUserByToken,
+  //     createUserAndGenerateToken,
 } = require("./db");
+
+//create a service using express
 const express = require("express");
+//the app in the service
 const app = express();
+//body parser
+app.use(express.json());
 
-const port = process.env.PORT || 3000;
+//are they logged in, yo?
+const isLoggedIn = async (req, res, next) => {
+  try {
+    console.log("req.headers", req.headers.authorization);
+    req.headers.authorization = req.headers.authorization.replace(
+      "Bearer ",
+      ""
+    );
+    req.user = await findUserByToken(req.headers.authorization);
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
 
+//--------------------routes--------------------
+//login- ------------REQUIRED ROUTE: 4
+app.post("/api/auth/login", async (req, res, next) => {
+  try {
+    console.log("req.body", req.body);
+    res.send(await authenticate(req.body));
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+//auth me route------------REQUIRED ROUTE: 5
+app.get("/api/auth/me", isLoggedIn, async (req, res, next) => {
+  try {
+    res.send(req.user);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+//register route------------REQUIRED ROUTE: 3
+app.post("/api/auth/register", async (req, res, next) => {
+  try {
+    res.send(await createUser(req.body));
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+//get items not logged in------------REQUIRED ROUTE: 1
+app.get("/api/items", async (req, res, next) => {
+  try {
+    res.send(await fetchItems());
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+//get a single item by param id not logged in ------------REQUIRED ROUTE: 2
+app.get("/api/items/:id", async (req, res, next) => {
+  try {
+    console.log("req.params.id", req.params.id);
+    res.send(await fetchItem(req.params.id));
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+// ---------------------------------------------REVIEW routes notlogged in
+//get reviews for an item not logged in------------REQUIRED ROUTE: 6
+app.get("/api/items/:id/reviews", async (req, res, next) => {
+  try {
+    res.send(await fetchItemReviews(req.params.id));
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+//get a single review for an item not logged in------------REQUIRED ROUTE: 7
+app.get("/api/items/:itemId/reviews/:id", async (req, res, next) => {
+  // console.log("req.params", req.params);
+  try {
+    res.send(
+      await fetchSingleReview({ itemId: req.params.itemId, id: req.params.id })
+    );
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+// ------------------------------------------------------------------REVIEW routes LOGGED-IN
+//create a review against an item and IS loggedIn REQUIRED ------------REQUIRED ROUTE: 8
+app.post("/api/items/:itemId/reviews", isLoggedIn, async (req, res, next) => {
+  console.log("req.user", req.user);
+  // console.log("req.params.itemId", req.params.itemId);
+  try {
+    res.status(201).send(
+      await createReview({
+        ...req.body,
+        userId: req.user.id,
+        itemId: req.params.itemId,
+      })
+    );
+  } catch (error) {
+    next(error);
+  }
+});
+
+// get all my reviews-------------------------REQUIRED ROUTE: 9
+app.get("/api/reviews/me", isLoggedIn, async (req, res, next) => {
+  console.log("req.user", req.user.id);
+  try {
+    res.send(await fetchMyReviews(req.user.id));
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+//allow logged in user to edit a review they created------------REQUIRED ROUTE: 10
+app.put(
+  "/api/users/:userId/reviews/:id",
+  isLoggedIn,
+  async (req, res, next) => {
+    try {
+      res.send(
+        await updateReview({
+          ...req.body,
+          userId: req.user.id,
+          id: req.params.id,
+        })
+      );
+    } catch (ex) {
+      next(ex);
+    }
+  }
+);
+
+//allow logged in user to delete a review they created------------REQUIRED ROUTE: 11
+app.delete(
+  "/api/users/:userId/reviews/:id",
+  isLoggedIn,
+  async (req, res, next) => {
+    try {
+      await deleteReview({ id: req.params.id, userId: req.user.id });
+      res.sendStatus(204);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// all the comment making editing and deleting while logged in shall be below
+//create a comment against a review------------REQUIRED ROUTE: 12
+app.post(
+  "/api/items/:id/reviews/:id/comments",
+  isLoggedIn,
+  async (req, res, next) => {
+    try {
+      res.status(201).send(
+        await createReviewComment({
+          ...req.body,
+          userId: req.user.id,
+          reviewId: req.params.id,
+          itemsId: req.params.id,
+        })
+      );
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+//error stuff
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500).send({ error: err.message || err });
+});
+
+//init it
 const init = async () => {
+  const port = process.env.PORT || 3000;
+
   await client.connect();
   //await the create tables
   await createTables();
   console.log("tables exist meow");
+
+  //create the data now, oy
+  const [
+    geologistgovern,
+    giganticugly,
+    artistetnt,
+    amanda,
+    betty,
+    carly,
+    dyson,
+    edison,
+    frankfurter,
+    washer,
+    dryer,
+    peacock,
+    troll,
+    hearts,
+    eyes,
+    pinkies,
+  ] = await Promise.all([
+    createUser({ username: "geologistgovern", password: "password" }),
+    createUser({ username: "giganticugly", password: "password" }),
+    createUser({ username: "artistetnt", password: "password" }),
+    createUser({ username: "amanda", password: "password" }),
+    createUser({ username: "betty", password: "password" }),
+    createUser({ username: "carly", password: "password" }),
+    createUser({ username: "dyson", password: "password" }),
+    createUser({ username: "edison", password: "password" }),
+    createUser({ username: "frankfurter", password: "password" }),
+    createItem({
+      name: "washer",
+      category: "clothes",
+      price: 1.99,
+      avgScore: 5,
+    }),
+    createItem({
+      name: "dryer",
+      category: "clothes",
+      price: 2.99,
+      avgScore: 4,
+    }),
+    createItem({
+      name: "peacock",
+      category: "animal",
+      price: 3.99,
+      avgScore: 3,
+    }),
+    createItem({
+      name: "troll",
+      category: "human",
+      price: 4.99,
+      avgScore: 2,
+    }),
+    createItem({
+      name: "hearts",
+      category: "bodypart",
+      price: 5.99,
+      avgScore: 1,
+    }),
+    createItem({
+      name: "eyes",
+      category: "bodypart",
+      price: 6.99,
+      avgScore: 10,
+    }),
+    createItem({
+      name: "pinkies",
+      category: "bodypart",
+      price: 7.99,
+      avgScore: 0,
+    }),
+  ]);
+  //fetch what we made
+  const users = await fetchUsers();
+  // console.log("fetched-users", users);
+
+  const items = await fetchItems();
+  // console.log("fetched-items", items);
+
+  //more creating data for a database in code
+  const [review1, review2, review3, review4, review5, review6, review7] =
+    await Promise.all([
+      createReview({
+        review: `good enough to eat`,
+        score: 5,
+        itemId: washer.id,
+        userId: geologistgovern.id,
+      }),
+      createReview({
+        review: `hard time flushing it`,
+        score: 4,
+        itemId: dryer.id,
+        userId: giganticugly.id,
+      }),
+      createReview({
+        review: `tastes great`,
+        score: 9,
+        itemId: dryer.id,
+        userId: artistetnt.id,
+      }),
+      createReview({
+        review: `less filling`,
+        score: 6,
+        itemId: dryer.id,
+        userId: betty.id,
+      }),
+      createReview({
+        review: `not worth the time`,
+        score: 3,
+        itemId: peacock.id,
+        userId: artistetnt.id,
+      }),
+      createReview({
+        review: `not worth the money`,
+        score: 2,
+        itemId: troll.id,
+        userId: amanda.id,
+      }),
+      createReview({
+        review: `not worth the effort`,
+        score: 1,
+        itemId: hearts.id,
+        userId: betty.id,
+      }),
+      createReview({
+        review: `not worth the energy`,
+        score: 10,
+        itemId: eyes.id,
+        userId: carly.id,
+      }),
+      createReview({
+        review: `not worth the pain`,
+        score: 0,
+        itemId: pinkies.id,
+        userId: dyson.id,
+      }),
+    ]);
+  //fetch some more stuff
+  const reviews = await fetchReviews();
+  // console.log("fetched-reviews", reviews);
+
+  //create comments against reviews via CODE! oy
+  const [comment1, comment2, comment3, comment4, comment5, comment6, comment7] =
+    await Promise.all([
+      createReviewComment({
+        comment: `liar`,
+        itemsId: washer.id,
+        userId: geologistgovern.id,
+        reviewId: review1.id,
+      }),
+      createReviewComment({
+        comment: `i agree`,
+        itemsId: dryer.id,
+        userId: giganticugly.id,
+        reviewId: review2.id,
+      }),
+      createReviewComment({
+        comment: `never has someone been more wrong`,
+        itemsId: dryer.id,
+        userId: artistetnt.id,
+        reviewId: review3.id,
+      }),
+      createReviewComment({
+        comment: `why me`,
+        itemsId: dryer.id,
+        userId: amanda.id,
+        reviewId: review4.id,
+      }),
+      createReviewComment({
+        comment: `will i ever know my bio-dad`,
+        itemsId: peacock.id,
+        userId: betty.id,
+        reviewId: review5.id,
+      }),
+      createReviewComment({
+        comment: `how many pigs are needed for a poke`,
+        itemsId: hearts.id,
+        userId: carly.id,
+        reviewId: review6.id,
+      }),
+      createReviewComment({
+        comment: `until we meet again`,
+        itemsId: eyes.id,
+        userId: dyson.id,
+        reviewId: review7.id,
+      }),
+    ]);
+  // await deleteReviewComment({ id: comment1.id, userId: geologistgovern.id });
 
   //connect that yo
   app.listen(port, () => {
